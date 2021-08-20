7755a50f10817ffe8aae6cec3e0cc3683711c876
cypx <cyprien.d@gmail.com>
2021-08-20 13:27:13 +0200

    add basic auth option

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"exporter/collector"
 	"log"
+	"crypto/subtle"
 	"net/http"
 	"os"
 
@@ -10,20 +11,46 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+func basicAuth(username string, password string, h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user, pass, ok := r.BasicAuth()
+
+		if !ok || subtle.ConstantTimeCompare([]byte(user), []byte(username)) != 1 || subtle.ConstantTimeCompare([]byte(pass), []byte(password)) != 1 {
+			w.Header().Set("WWW-Authenticate", `Basic realm="metrics"`)
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("Unauthorised\n"))
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	})
+}
+
 func main() {
-	IP := ""
-	PORT := ""
-	if len(os.Args) < 2 {
-		IP = "127.0.0.1"
-		PORT = "8080"
-	} else {
+	IP := "127.0.0.1"
+	PORT := "8080"
+	basicAuthUser := ""
+	basicAuthPass := ""
+	enableAuth := false
+	if len(os.Args) > 2 {
 		IP = os.Args[1]
 		PORT = os.Args[2]
+	}
+	if len(os.Args) > 3 {
+		basicAuthUser = os.Args[3]
+		basicAuthPass = os.Args[4]
+		enableAuth = true
 	}
 
 	foo := collector.NewwRamCollector()
 	prometheus.MustRegister(foo)
-	lisen := IP + ":" + PORT
-	http.Handle("/metrics", promhttp.Handler())
-	log.Fatal(http.ListenAndServe(lisen, nil))
+	listen := IP + ":" + PORT
+
+	if enableAuth {
+		http.Handle("/metrics", basicAuth(basicAuthUser, basicAuthPass, promhttp.Handler()))
+	} else {
+		http.Handle("/metrics", promhttp.Handler())
+	}
+
+	log.Fatal(http.ListenAndServe(listen, nil))
 }
