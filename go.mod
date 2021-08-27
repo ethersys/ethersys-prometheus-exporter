47cd99ba13e4876cfd81ef6a566904c8fdd7a00f
cyprien <cyprien@ethersys.fr>
2021-08-27 12:28:35 +0200

    filterprocess with go package

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/prometheus/client_golang v1.11.0
+	github.com/shirou/gopsutil/v3 v3.21.7 // indirect
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 )
--- script/script.go
+++ script/script.go
@@ -1,11 +1,25 @@
 package script
 
 import (
+	"fmt"
 	"log"
 	"os/exec"
 	"strconv"
 	"strings"
+	"github.com/shirou/gopsutil/v3/process"
 )
+
+func ProcessFilter(processname string) ([]*process.Process, error) {
+	var ret []*process.Process
+	processes, _ := process.Processes()
+	for i, proc := range processes {
+		name, _ := proc.Name()
+		if strings.Contains( name, processname) {
+			ret = append(ret, proc)
+		}
+	}
+	return ret, nil
+}
 
 func UsedMemory(soft string) float64 {
 	commande := "echo $(ps aux | grep \"" + soft + "\"| grep -v grep | awk 'BEGIN { sum=0 } {sum=sum+$6; } END {printf(\"%s\",sum / 1024)}')"
