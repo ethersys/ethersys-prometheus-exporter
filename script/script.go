29020cc0a53a1ed4aa9bb66fc3af20946c25f1e0
cyprien <cyprien@ethersys.fr>
2021-08-30 09:50:22 +0200

    fetch memory usage with go package

--- script/script.go
+++ script/script.go
@@ -2,9 +2,6 @@ package script
 
 import (
 	"strings"
-	"log"
-	"os/exec"
-	"strconv"
 	"github.com/shirou/gopsutil/v3/process"
 )
 
@@ -20,28 +17,17 @@ func ProcessFilter(processname string) ([]*process.Process, error) {
 	return ret, nil
 }
 
-func UsedMemoryPercent(soft string) float64 {
+func UsedMemory(soft string) float64 {
 	var ret float64 = 0
-	var val float32
-	processes, _ := ProcessFilter(soft)
+	var val float64
+	var mem *process.MemoryInfoStat
+	processes, _ := process.Processes()
 	for _, proc := range processes {
-		val, _ = proc.MemoryPercent()
-		ret += float64(val)
+		mem, _ = proc.MemoryInfo()
+		val = float64(mem.RSS)
+		ret += val / 1024
 	}
 	return ret
-}
-
-func UsedMemory(soft string) float64 {
-	commande := "echo $(ps aux | grep \"" + soft + "\"| grep -v grep | awk 'BEGIN { sum=0 } {sum=sum+$6; } END {printf(\"%s\",sum / 1024)}')"
-	cmd, err := exec.Command("bash", "-c", commande).Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	res, err := strconv.ParseFloat(strings.Replace(string(cmd), "\n", "", 1), 64)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return res
 }
 
 func UsedCPU(soft string) float64 {
